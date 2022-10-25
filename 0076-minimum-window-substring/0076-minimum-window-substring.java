35f3ec12bb8a81da409d81b9d7b44c3a8baf4c21
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-10-25 12:52:07 +0530

    Time: 43 ms (18.97%), Space: 54.1 MB (13.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,54 +1,71 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
-        if (t.length() > s.length() || t.equals(""))
-            return "";
-        
-        Map<Character, Integer> tMap = new HashMap<>();
-        Map<Character, Integer> sMap = new HashMap<>();
-        
-        for(char c : t.toCharArray()){
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-            sMap.put(c, 0);
-        }
-        
-        int start=0, matched=0;
-        int minLength = s.length();
-        int[] result = new int[]{-1,-1};
-		
-        for (int end=0; end < s.length(); end++){
-            char curr = s.charAt(end);
-            
-            // Check if current char is present in target, then update character count in source map
-            if(tMap.containsKey(curr)){
-                sMap.put(curr, sMap.get(curr) + 1);            
-                if(sMap.get(curr).equals(tMap.get(curr)))
-                    matched++;
-            }
-            
-            // When a valid window is found, try to reduce it until a valid window is not found
-            if(matched == tMap.size()){
-                // Iterate until the window is not valid
-                while(matched == tMap.size()){
-                    char first = s.charAt(start++);
-                    // If current char is a character in target, 
-                    // then reduce its count from the sMap as we'll reduce its count
-                    if(sMap.containsKey(first)){
-                        sMap.put(first, sMap.get(first) - 1);
-                        // If count of first char in window < count in tMap, then reduce the matched count
-                        if(sMap.get(first) < tMap.get(first))
-                            matched--;
-                    }
-                }
-                // Update length if current length is smaller than previous
-                // Note - Check 1 character before start index as the start idx was already incremented in the while loop
-                if (end - (start - 1) < minLength){
-                    minLength = end - (start - 1) + 1;
-                    result[0] = start - 1;
-                    result[1] = end;
-                }
-            }
-        }
-        return result[0] != -1 ? s.substring(result[0], result[1] + 1) : "";
+    String result = "";
+    int resultCount = Integer.MAX_VALUE;
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    for(char a:t.toCharArray())
+    	map.put(a, map.getOrDefault(a, 0)+1);
+    
+    int i = 0;
+    int j = 0;
+    int currentCount = 0;
+    HashMap<Character, Integer> currentMap = new HashMap<Character, Integer>();
+    while(i<s.length()|| (j<s.length() && currentCount == t.length()))
+    {
+    	
+    	
+    	if(currentCount == t.length() && j<s.length())
+    	{
+            char ch1 = s.charAt(j);
+    			if(currentMap.containsKey(ch1))
+    			{
+    				if(currentMap.get(ch1) == 1)
+    				{
+    					currentMap.remove(ch1);
+    					currentCount--;
+    				}
+    				else {
+    					currentMap.put(ch1, currentMap.get(ch1)-1);
+    					if(currentMap.get(ch1)<map.get(ch1))
+    						currentCount--;
+    				}
+    				
+    			}
+            j++;
+    	}
+    	else if(i<s.length()) {
+            char ch = s.charAt(i);
+    		if(map.containsKey(s.charAt(i)))
+    		{
+    			currentMap.put(ch, currentMap.getOrDefault(ch, 0)+1);
+    			if(currentMap.get(ch)<= map.get(ch))
+    			{
+    				currentCount++;
+    			}
+    		}
+    		
+    		i++;
+    	}
+       //System.out.println("currentCount -->"+currentCount);
+      //  System.out.println("i -->"+i);
+      //  System.out.println("j -->"+j);
+    	if(currentCount == t.length())
+    	{
+            // System.out.println("currentSubstring -->"+s.substring(j, i));
+    		if(resultCount>(i-j))
+    		{
+    			resultCount = (i-j);
+    			result = s.substring(j, i);
+    		}
+    	}
+         
+        //System.out.println("resultCount -->"+resultCount);
+       //  System.out.println("result -->"+result);
+       //  System.out.println("------------->");
+    	
+    	
+    	
     }
+    return result;
+}
 }
