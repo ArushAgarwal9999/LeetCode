237c1dc5aa5d4ebeb01d517bd06cfb627d6690ec
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-02 18:50:09 +0530

    Time: 2 ms (55.26%), Space: 42 MB (49.55%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -4,16 +4,15 @@ class Solution {
 		 for(String a:bank)
 			 map.put(a, false);
 		 
-		 int res = getMinMutation(start, end, map);
+		 int res = getMinMutation(start, end, map,new HashMap<String, Integer>());
 		 if(res>=100000)
 	        return -1;
 		 return res;
 	    }
-	 public int getMinMutation(String start, String end, HashMap<String, Boolean> map)
+	 public int getMinMutation(String start, String end, HashMap<String, Boolean> map,HashMap<String, Integer> dp)
 	 {
 		 if(start.equals(end))
 			 return 0;
-		 
 		 
 		 char[]ch = {'A', 'C', 'G', 'T'};
 		 int result = 100000;
@@ -30,7 +29,7 @@ class Solution {
 					 if(map.containsKey(newStart) && !map.get(newStart))
 					 {
 						 map.put(newStart, true);
-						 result = Math.min(getMinMutation(newStart, end, map)+1, result);
+						 result = Math.min(getMinMutation(newStart, end, map,dp)+1, result);
 						 
 						 map.put(newStart, false);
 						 
@@ -39,7 +38,7 @@ class Solution {
 				 }
 			 }
 		 }
-		
+		dp.put(start,result);
 		 
 		 
 		 
