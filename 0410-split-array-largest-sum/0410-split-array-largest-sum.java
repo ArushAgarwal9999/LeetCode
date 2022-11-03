2e5d8f6de31630a4993c802bf3a83ec747af721d
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-03 17:44:11 +0530

    Time: 609 ms (5.00%), Space: 44 MB (5.12%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-		long res = minSplitArray(nums, m, 0, new Long[nums.length][m+1]);
-		return (int ) res;
+		int res = minSplitArray(nums, m, 0, new Integer[nums.length][m+1]);
+		return  res;
         
     }
-public long minSplitArray(int[] nums, int m, int index, Long[][] dp)
+public int minSplitArray(int[] nums, int m, int index, Integer[][] dp)
 {
 	if(index >= nums.length || m ==1)
 	{
 		if(m>0 && index>= nums.length)
 		{
-			return 100000000l;
+			return Integer.MAX_VALUE;
 		}
 		
-		long result = 0;
+		int result = 0;
 		while(index<nums.length)
 		{
 			result+=nums[index];
@@ -25,18 +25,15 @@ public long minSplitArray(int[] nums, int m, int index, Long[][] dp)
 	if(dp[index][m] != null)
 		return dp[index][m];
 	
-	long result = Long.MAX_VALUE;
-	long res =0;
+	int result = Integer.MAX_VALUE;
+	int res =0;
 	for(int i = index;i<nums.length;i++)
 	{
 		res+= nums[i];
         
-        long res2 = minSplitArray(nums,m-1, i+1, dp);
-       // System.out.println("nums[i] -->>"+nums[i]);
-       // System.out.println("res -->>"+res);
-       // System.out.println("res2 -->>"+res2);
+        int res2 = minSplitArray(nums,m-1, i+1, dp);
+      
 		result = Math.min(result, Math.max(res,res2));
-        // System.out.println("result -->>"+result);
 	}
 	
 	return dp[index][m] = result;
