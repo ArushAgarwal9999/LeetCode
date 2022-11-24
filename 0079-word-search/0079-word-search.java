befe273d9315ee004751fe0ee0455582cd749723
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-24 08:14:41 +0530

    Time: 214 ms (35.14%), Space: 42.2 MB (46.21%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,40 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-	for(int i = 0;i<board.length;i++)
-	{
-		for(int j = 0;j<board[i].length;j++)
-		{
-			if(board[i][j]==word.charAt(0))
-			{
-				if(dfs_findWord(i, j, 0, new Boolean[board.length][board[0].length], board, word))
-					return true;
-			}
-		}
-	}
-        return false;
+     for(int i = 0;i<board.length;i++)
+     {
+    	 for(int j = 0;j<board[i].length;j++)
+    	 {
+    		 if(dfs(0, i, j, board, word, new Boolean[board.length][board[0].length]))
+    			 return true;
+    	 }
+     }
+	
+	return false;
     }
-public boolean dfs_findWord(int i, int j, int index, Boolean [][] visited,char[][] board,String word)
+public boolean dfs(int index, int row, int column, char[][] board, String word,  Boolean[][] visited)
 {
 	if(index>=word.length())
-		return true;
-	if(i<0||i>=board.length || j<0|| j>=board[0].length || visited[i][j] != null)
+    {
+        return true;
+        
+    }
+   
+		
+	if(row<0||row>=board.length||column<0||column>=board[0].length|| visited[row][column]!= null || word.charAt(index)!=board[row][column])
 		return false;
 	
-	if(word.charAt(index) != board[i][j])
-		return false;
-	
-	visited[i][j]= true;
-	
-	if (dfs_findWord(i+1, j, index+1, visited, board, word) || dfs_findWord(i-1, j, index+1, visited, board, word) || dfs_findWord(i, j+1, index+1, visited, board, word)|| dfs_findWord(i, j-1, index+1, visited, board, word))
+   
+        visited[row][column] = true;
+	if(dfs(index+1, row-1, column, board, word, visited)||dfs(index+1, row+1, column, board, word, visited)||dfs(index+1, row, column-1, board, word, visited)||dfs(index+1, row, column+1, board, word, visited))
 	{
-		visited[i][j]= null;
+		visited[row][column] = null;
 		return true;
 	}
-    visited[i][j]= null;
+	    visited[row][column] = null;
+    
+	
 	return false;
+	
 }
+
 }
