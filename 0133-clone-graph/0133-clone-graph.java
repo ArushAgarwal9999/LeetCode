450f9de64610aca4d3e543891c246db08e60a7f8
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-25 09:31:19 +0530

    Time: 25 ms (99.65%), Space: 42.5 MB (86.94%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,43 +19,36 @@ class Node {
 */
 
 class Solution {
-   public Node cloneGraph(Node node) {
-       if(node==null)
-           return null;
-		HashSet<Node> visited = new HashSet<Node>();
-		HashMap<Node, ArrayList<Node>> map = new HashMap<Node, ArrayList<Node>>();
-		HashMap<Integer, Node> cloneMap = new HashMap<Integer, Node>();
-		dfs(node, null, map, visited,cloneMap);
-		//System.out.println(map);
-		//System.out.println(cloneMap);
-        return cloneMap.get(node.val);
-    }
-	public void dfs(Node current, Node prev,HashMap<Node, ArrayList<Node>> map,HashSet<Node> visited,HashMap<Integer, Node> cloneMap)
-	{
-		if(!map.containsKey(current))
+  public Node cloneGraph(Node node) {
+	       if(node==null)
+	           return null;
+			HashSet<Node> visited = new HashSet<Node>();
+			HashMap<Integer, Node> cloneMap = new HashMap<Integer, Node>();
+			dfs(node, null, visited,cloneMap);
+			//System.out.println(map);
+			//System.out.println(cloneMap);
+	        return cloneMap.get(node.val);
+	    }
+		public void dfs(Node current, Node prev,HashSet<Node> visited,HashMap<Integer, Node> cloneMap)
 		{
-			map.put(current, new ArrayList<Node>());
-		}
-		if(!cloneMap.containsKey(current.val))
-		{
-			Node clone = new Node(current.val);
-			cloneMap.put(current.val, clone);
-		}
-		visited.add(current);
-		for(Node n:current.neighbors)
-		{
-			if(!cloneMap.containsKey(n.val))
+			if(!cloneMap.containsKey(current.val))
 			{
-				Node clone = new Node(n.val);
-				cloneMap.put(n.val, clone);
+				Node clone = new Node(current.val);
+				cloneMap.put(current.val, clone);
 			}
-			cloneMap.get(current.val).neighbors.add(cloneMap.get(n.val)) ;
-			
-			
-			map.get(current).add(n);
-			if(visited.contains(n)|| (prev!= null && prev==n))
-				continue;
-			dfs(n, current, map, visited,cloneMap);
+			visited.add(current);
+			for(Node n:current.neighbors)
+			{
+				if(!cloneMap.containsKey(n.val))
+				{
+					Node clone = new Node(n.val);
+					cloneMap.put(n.val, clone);
+				}
+				cloneMap.get(current.val).neighbors.add(cloneMap.get(n.val)) ;
+				
+				if(visited.contains(n)|| (prev!= null && prev==n))
+					continue;
+				dfs(n, current, visited,cloneMap);
+			}
 		}
-	}
 }
