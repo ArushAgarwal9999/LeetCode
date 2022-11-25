9edf8da8eb85f52ac03b64bea4b7ab59a331a623
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-25 12:41:09 +0530

    Time: 52 ms (32.01%), Space: 44.7 MB (28.59%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,34 @@
+class Solution {
+          public String mostCommonWord(String paragraph, String[] banned) {
+                    HashSet<String> bannedSet = new HashSet<>();
+                for(String word:banned)
+                    bannedSet.add(word.toLowerCase());
+                 paragraph = paragraph.replaceAll("[!?',;.]"," ");
+        String [] words = paragraph.split(" ");
+        HashMap<String,Integer> map = new HashMap<>();
+        String result = "";
+        int mostFrequancy=0;
+        System.out.println("paragraph -->>"+paragraph);
+        System.out.println("words -->>"+Arrays.toString(words));
+        for(String word:words)
+        {
+             word = word.toLowerCase().trim();
+            word= word.trim();
+            if(bannedSet.contains(word) || word.trim().isEmpty())
+                continue;
+           
+            System.out.println("word -->>"+word);
+            
+            map.put(word,map.getOrDefault(word,0)+1);
+            if(mostFrequancy<map.get(word))
+            {
+            mostFrequancy = map.get(word);
+        result = word;	
+            }
+        }
+        return result;
+
+
+            }
+
+}
