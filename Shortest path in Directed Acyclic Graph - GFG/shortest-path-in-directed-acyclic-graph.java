4bdf8526d68b2a5b523af226f29708817031d78a
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-26 17:07:40 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
@@ -30,81 +30,53 @@ class Main {
 
 //User function Template for Java
 class Solution {
-    class Par{
-		int des;
-		int weight;
-		public Par(int des, int weight) {
-			super();
-			this.des = des;
-			this.weight = weight;
-		}
-		@Override
-			public String toString() {
-				return "Par [des=" + des + ", weight=" + weight + "]";
-			}
-	}
 
-	public int[] shortestPath(int N,int M, int[][] edges) {
-		//Code here
-		int[] result = new int[N];
-		int[] indegree = new int[N];
-		Arrays.fill(result, Integer.MAX_VALUE);
-		ArrayList<ArrayList<Par>> adj = new ArrayList<ArrayList<Par>>();
-		for(int i = 0;i<N;i++)
-		{
-			adj.add(new ArrayList<Par>());
-		}
-		for(int i = 0;i<edges.length;i++)
-		{
-			adj.get(edges[i][0]).add(new Par(edges[i][1], edges[i][2]));
-			indegree[edges[i][1]]++;
-		}
-// 		System.out.println("indegree -->>"+Arrays.toString(indegree));
-		Queue<Integer> quee = new LinkedList<Integer>();
-		for(int i = 0;i<indegree.length;i++)
-		{
-			if(indegree[i]==0)
-				quee.add(i);
-		}
-// 		System.out.println("quee -->>"+quee);
-		Stack<Integer> inorder = new Stack<Integer>();
-		while(!quee.isEmpty())
-		{
-			int a = quee.poll();
-// 			System.out.println("a -->>"+a);
-			inorder.add(a);
-			for(Par num:adj.get(a))
-			{
-				indegree[num.des]--;
-				// System.out.println("indegree 1-->>"+Arrays.toString(indegree));
-				if(indegree[num.des]==0)
-					quee.add(num.des);
+class Pair{
+			@Override
+			public String toString() {
+				return "Pair [des=" + des + ", weight=" + weight + "]";
+			}
+			int des;
+			int weight;
+			public Pair(int des, int weight) {
+				super();
+				this.des = des;
+				this.weight = weight;
 			}
 		}
-		result[0] = 0;
-// 			System.out.println("inorder -->>"+inorder);
-		while(!inorder.isEmpty())
-		{
-			int num =inorder.remove(0);
-// 			System.out.println("num -->>"+num);
-			int currentdis = result[num];
-			for(Par par:adj.get(num))
+		public int[] shortestPath(int N,int M, int[][] edges) {
+			int[] result = new int[N];
+			Arrays.fill(result, Integer.MAX_VALUE);
+			ArrayList<ArrayList<Pair>> adj = new ArrayList<ArrayList<Pair>>();
+			for(int i = 0;i<N;i++)
 			{
-			 //   System.out.println("par -->>"+par);
-			 //   System.out.println("current weight -->>"+(currentdis+par.weight));
-				if( currentdis !=Integer.MAX_VALUE && result[par.des]>(currentdis+par.weight) )
+				adj.add(new ArrayList<Pair>());
+			}
+			for(int i = 0;i<edges.length;i++)
+			{
+				adj.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
+			}
+			result[0] = 0;
+			Queue<Pair> quee = new PriorityQueue<Pair>((a,b)->a.weight-b.weight);
+			quee.add(new Pair(0, 0));
+			while(!quee.isEmpty())
+			{
+				Pair p = quee.poll();
+				for(Pair num:adj.get(p.des))
 				{
-					result[par.des] = currentdis+par.weight;
+					if((p.weight+num.weight)<result[num.des])
+					{
+						result[num.des]= (p.weight+num.weight);
+						quee.add(new Pair(num.des, p.weight+num.weight));
+					}
 				}
-				// System.out.println("result -->>"+Arrays.toString(result));
 			}
+			for(int i = 0;i<result.length;i++)
+			{
+				if(result[i] ==Integer.MAX_VALUE)
+					result[i] =-1;
+			}
+			
+			return result;
 		}
-		for(int i = 0;i<result.length;i++)
-		{
-			if(result[i]==Integer.MAX_VALUE)
-				result[i]=-1;
-		}
-		
-		return result;
-	}
 }
