a3141a583dace859a00d47310b1f832567c7a023
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-28 09:12:53 +0530

    Time: 393 ms (17.95%), Space: 143.7 MB (74.34%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,19 +1,16 @@
 class Solution {
-    public List<List<Integer>> findWinners(int[][] matches) {
+   public List<List<Integer>> findWinners(int[][] matches) {
 	List<List<Integer>> result = new ArrayList<List<Integer>>();
 	TreeSet<Integer> set = new TreeSet<Integer>();
 	result.add(new ArrayList<Integer>());
 	result.add(new ArrayList<Integer>());
-	HashMap<Integer, ArrayList<Integer>> loss =new HashMap<Integer, ArrayList<Integer>>();
+	HashMap<Integer, Integer> loss =new HashMap<Integer, Integer>();
 	for(int[] a:matches)
 	{
 		set.add(a[0]);
 		set.add(a[1]);
-		if(!loss.containsKey(a[1]))
-		{
-			loss.put(a[1], new ArrayList<Integer>());
-		}
-		loss.get(a[1]).add(a[0]);
+		
+		loss.put(a[1], loss.getOrDefault(a[1], 0)+1);
 	}
 		for(int a:set)
 		{
@@ -22,7 +19,7 @@ class Solution {
 				result.get(0).add(a);
 			}
 			else {
-				if(loss.get(a).size()==1)
+				if(loss.get(a)==1)
 				{
 					result.get(1).add(a);
 				}
