3a9f249be1238836de4bedc3528c15b3ceaa7b0d
Arush <49750288+ArushAgarwal9999@users.noreply.github.com>
2022-11-30 10:31:16 +0530

    Time: 122 ms (40.23%), Space: 67.1 MB (28.92%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,52 +1,54 @@
 class Solution {
-    class Pair{
-		int row;
+   class Pair{
+		int row ;
 		int column;
 		int diff;
-		public Pair(int row,int column, int diff) {
+		@Override
+		public String toString() {
+			return "Pair [row=" + row + ", column=" + column + ", diff=" + diff + "]";
+		}
+		public Pair(int row, int column, int diff) {
 			super();
 			this.row = row;
 			this.column = column;
 			this.diff = diff;
 		}
-        @Override
-		public String toString() {
-			return "Pair [row=" + row + ", column=" + column + ", diff=" + diff + "]";
-		}
-		
-		
 	}
-   public int minimumEffortPath(int[][] heights) {
-	boolean[][] visited = new boolean[heights.length][heights[0].length];
+public int minimumEffortPath(int[][] heights) {
+	
+	int[][] ditance = new int[heights.length][heights[0].length];
+	for(int[]a:ditance)
+		Arrays.fill(a, Integer.MAX_VALUE);
 	Queue<Pair> quee = new PriorityQueue<Pair>((a,b)->a.diff-b.diff);
+	ditance[0][0] = 0;
 	quee.add(new Pair(0, 0, 0));
 	while(!quee.isEmpty())
 	{
 		//System.out.println("quee -->>"+quee);
-		
 		Pair p = quee.poll();
-        visited[p.row][p.column] = true;
-		//System.out.println("pair p -->>"+p);
-		if(p.row==heights.length-1 && p.column==heights[0].length-1)
+		if(p.row == heights.length-1 && p.column==heights[0].length-1)
 		{
 			return p.diff;
 		}
-		int[] rowMove = {-1,1,0,0};
-		int[] columnMove = {0,0,-1,1};
-		for(int i = 0;i<rowMove.length;i++)
+		int[] row = {-1,1,0,0};
+		int[] column = {0,0,-1,1};
+		for(int i = 0;i<row.length;i++)
 		{
-			int newRow = p.row+rowMove[i];
-			int newColumn= p.column+columnMove[i];
-			if(newRow>=0 && newRow<heights.length&&newColumn>=0&& newColumn<heights[0].length && visited[newRow][newColumn]==false)
+			int newRow = row[i]+p.row;
+			int newcolumn = column[i]+p.column;
+			if(newcolumn>=0&& newcolumn<heights[0].length&& newRow>=0 && newRow<heights.length)
 			{
-				//visited[newRow][newColumn] = true;
-				quee.add(new Pair(newRow, newColumn, Math.max(Math.abs(heights[newRow][newColumn]-heights[p.row][p.column]), p.diff)));
+				int dis = Math.max(p.diff,  Math.abs(heights[p.row][p.column]-heights[newRow][newcolumn]));
+				if(dis<ditance[newRow][newcolumn])
+				{
+                    ditance[newRow][newcolumn] = dis;
+					quee.add(new Pair(newRow, newcolumn,dis));
+				}
 				
 			}
-			
 		}
-		
 	}
-        return 0;
+	return 0;
+        
     }
 }
